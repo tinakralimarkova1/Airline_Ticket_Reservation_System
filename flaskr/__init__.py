eeee64a2df34fbce11eab7b139fe510c1de42c43
tinakralimarkova1 <67720045+tinakralimarkova1@users.noreply.github.com>
2025-12-03 09:45:59 +0800

    Staff can log in and register

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -70,7 +70,7 @@ def login_cust():
         cursor = conn.cursor()
 
         #query and fetching data
-        query = "SELECT Email, Password FROM Customer WHERE Email = %s and Password = %s"
+        query = "SELECT email, password FROM Customer WHERE email = %s and password = %s"
         cursor.execute(query, (username, password))
         data = cursor.fetchone()
         cursor.close()
@@ -86,12 +86,62 @@ def login_cust():
             return render_template('login_cust.html', error=error)
 
     return render_template('loginCust.html')
-@app.route('/login_agent')
+@app.route('/login_agent', methods=['GET', 'POST'])
 def login_agent():
+    if request.method == 'POST':
+
+        #user input 
+        username = request.form['username']
+        password = request.form['password']
+
+        #connect to dp
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        #query and fetching data
+        query = "SELECT email, password FROM booking_agent WHERE email = %s and password = %s"
+        cursor.execute(query, (username, password))
+        data = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+
+        #if data is found, username exists 
+        if data:
+            session['username'] = username
+            return redirect(url_for('agent_home'))
+        else:
+            error = 'Invalid login or username'
+            return render_template('login_agent.html', error=error)
     return render_template('loginAgent.html')
 
-@app.route('/login_staff')
+@app.route('/login_staff', methods=['GET', 'POST'])
 def login_staff():
+    if request.method == 'POST':
+
+        #user input 
+        username = request.form['username']
+        password = request.form['password']
+
+        #connect to dp
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        #query and fetching data
+        query = "SELECT username, password FROM airline_staff WHERE username = %s and password = %s"
+        cursor.execute(query, (username, password))
+        data = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+
+        #if data is found, username exists 
+        if data:
+            session['username'] = username
+            return redirect(url_for('staff_home'))
+        else:
+            error = 'Invalid login or username'
+            return render_template('loginStaff.html', error=error)
     return render_template('loginStaff.html')
 
 #register
@@ -168,11 +218,64 @@ def register_customer():
 def register_agent():
     return render_template('registerAgent.html')
 
+
+@app.route('/register_staff', methods=['GET', 'POST'])
+def register_staff():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        date_of_birth = request.form.get('date_of_birth')
+
+
+        if not all([username, password, first_name, last_name, date_of_birth]):
+            error = "All fields are required"
+            return render_template('registerStaff.html', error=error)
+    
+
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        query = "SELECT username FROM airline_staff WHERE username = %s"
+        cursor.execute(query, (username,))
+        data = cursor.fetchone()
+
+        if data:
+            error = "This username is already in use"
+            return render_template('registerStaff.html', error=error)
+        else:
+            # Insert the new customer record
+            query = """INSERT INTO airline_staff (username,password, first_name, last_name, date_of_birth) 
+                       VALUES (%s, %s, %s, %s, %s)"""
+            cursor.execute(query, (username, password, first_name, last_name, date_of_birth))
+            conn.commit()
+            cursor.close()
+            conn.close()
+
+            return redirect(url_for('login_staff'))
+
+    return render_template('registerStaff.html')
+
 #customer 
 
 @app.route('/customer_home')
 def customer_home():
     return render_template('indexCust.html')
+
+
+#booking agent
+
+@app.route('/agent_home')
+def agent_home():
+    return render_template('indexAgent.html')
+
+#airline staff
+
+@app.route('/staff_home')
+def staff_home():
+    return render_template('indexStaff.html')
 
 
 #logout
